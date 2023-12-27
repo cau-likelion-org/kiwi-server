a6db1166a2cc9746b1e0d0cacc61b92842a04c33
jeonghens <jhyeonseo0523@gmail.com>
2023-12-27 17:49:43 +0900

    feat: history 관련 urls 및 views 업데이트

--- config/urls.py
+++ config/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('users/', include('allauth.urls')),
+    path('history/', include('documents.urls')),
 ]
