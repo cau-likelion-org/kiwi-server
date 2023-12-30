5cedf404a41b326b2d24337d9b5912a699d968d1
jeonghens <jhyeonseo0523@gmail.com>
2023-12-30 15:34:28 +0900

    fix: docs, history urlpatterns 분리

--- documents/urls.py
+++ documents/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import *
 
-urlpatterns = [
+docs_urlpatterns = [
     path('', HistoryDocCreateAPI.as_view(), name = '문서 작성'),
     path('recent/<str:title>/', DocDetailAPI.as_view(), name = '특정 문서 중 가장 최근 편집된 글 가져오기'),
     path('random/', RandomDocAPI.as_view(), name = '임의 문서 가져오기'),    
@@ -9,7 +9,9 @@ urlpatterns = [
     path('search/<str:keyword>/', SearchHistoryDocAPI.as_view(), name="문서 검색"),
     path('backlink/<str:title>/', BackLinkAPI.as_view(), name='역링크 조회'),
     path('image/',ImageUploadView.as_view(), name='이미지 업로드' ),
+]
 
+history_urlpatterns = [
     path('recent/', RecentEditedDocumentsAPI.as_view(), name='최근 편집된 전체 문서 목록'),
     path('<str:title>/', DocumentEditHistoryAPI.as_view(), name='특정 문서의 전체 편집 목록'),
     path('<str:title>/detail/', DocumentEditComparisonAPI.as_view(), name='특정 문서의 편집 변경 사항'), 
