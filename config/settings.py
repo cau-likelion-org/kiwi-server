8524565d545962a255b17c88b90878bdfa6ed50b
hnnynh <hnnynh125@gmail.com>
2024-06-09 22:21:40 +0900

    fix: POST body parse

--- config/settings.py
+++ config/settings.py
@@ -65,6 +65,7 @@ THIRD_PARTY_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'rest_framework_simplejwt',
+    'rest_framework_simplejwt.token_blacklist',
     'dj_rest_auth',
     'dj_rest_auth.registration',
 
