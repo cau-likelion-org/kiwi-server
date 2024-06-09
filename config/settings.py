cc0551f16acb616058d456a95b0ca6c1bde083f5
Yoonho Hann <83288181+hnnynh@users.noreply.github.com>
2024-06-09 17:59:58 +0900

    hotfix: db migration

--- config/settings.py
+++ config/settings.py
@@ -135,7 +135,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
 	'default': {
 		'ENGINE': 'django.db.backends.mysql',
-		'NAME': 'mutsa_wiki_db',
+		'NAME': 'llwiki',
 		'USER': 'admin',
 		'PASSWORD': get_secret("DB_PASSWORD"),
 		'HOST': get_secret("DB_HOST"),
